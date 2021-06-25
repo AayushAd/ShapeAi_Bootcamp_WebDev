21abec94f1470599647640126ceabbd96ef2a0a9
AayushAd <72729234+AayushAd@users.noreply.github.com>
2021-06-25 16:08:30 +0545

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>SHAPE AI BOOTCAMPS</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic Web Dev React.js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
